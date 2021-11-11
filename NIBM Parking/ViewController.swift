20ce252bd2b041e8d6bdaa8dab8bb73e535ef615
Ruchira Wishwajith <ruchiraherath8@gmail.com>
2021-11-11 12:54:35 +0530

    Initial Commit

---
+++ NIBM Parking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NIBM Parking
+//
+//  Created by Dilan Pramodya on 2021-11-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
