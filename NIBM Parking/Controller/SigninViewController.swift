23bf419d51fceb7c9f1aabbba922f661f567f995
Ruchira Wishwajith <ruchiraherath8@gmail.com>
2021-11-12 21:27:40 +0530

    Linking Controllers and Main Storyboards

---
+++ NIBM Parking/Controller/SigninViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SigninViewController.swift
+//  NIBM Parking
+//
+//  Created by Dilan Pramodya on 2021-11-12.
+//
+
+import UIKit
+
+class SigninViewController: UIViewController
+{
+
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+
+    @IBAction func onSignInPressed(_ sender: UIButton) {
+        
+        if txtEmail.text?.count < 5
+        {
+            print ("Please Enter a Valid Password!!")
+        }
+        
+        
+        
+    }
+    
+  
+}
---
+++ NIBM Parking/Model/EntityModel.swift
@@ -0,0 +1,26 @@
+//
+//  EntityModel.swift
+//  NIBM Parking
+//
+//  Created by Ruchira Wishwajith on 2021-11-12.
+//
+
+import Foundation
+
+struct PakingUser
+{
+    var Username: String
+    var UserEmail: String
+    var Usermobilenumber: Int
+    var UserPassword: String
+    var NIBMRegNumber: String
+    
+    
+}
+
+struct VehicalDetails
+{
+    var VehicleNumber: String
+    var Vehicalbrand: String
+    var VehicalModal: String
+}
