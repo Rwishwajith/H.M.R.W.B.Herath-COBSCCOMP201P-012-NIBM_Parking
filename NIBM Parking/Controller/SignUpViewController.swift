959c8ab4648355cd3f91716ca7508259362bcfd7
Ruchira Wishwajith <ruchiraherath8@gmail.com>
2021-11-16 21:22:04 +0530

    Sign Up with Firebase Authentication

--- NIBM Parking/Controller/SignUpViewController.swift
+++ NIBM Parking/Controller/SignUpViewController.swift
@@ -6,24 +6,115 @@
 //
 
 import UIKit
+import Firebase
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var txtusername: UITextField!
+    @IBOutlet weak var txtemail: UITextField!
+    @IBOutlet weak var txtphonenumber: UITextField!
+    @IBOutlet weak var txtpassword: UITextField!
+    @IBOutlet weak var txtnibmid: UITextField!
+    @IBOutlet weak var txtvehicalNumber: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func regsiteruser(email: String, password: String)
+    {
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error{
+                print(error.localizedDescription)
+                return
+            }
+            if let result = authResult
+            {
+                print ("New User Registerd: \(result.user.email ?? "Cannot be found")")
+            }
+            
+        }
     }
-    */
+
+    @IBAction func onSignUpPressed(_ sender: UIButton)
+    {
+     if inputValidation()
+     {
+        regsiteruser(email: txtemail.text!, password: txtpassword.text!)
+     }
+     else
+     {
+        print("Invalid data found in the given details")
+     }
+    }
+    @IBAction func signUpPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+   
+    
+    func inputValidation() -> Bool
+    {
+        guard let username = txtusername.text else {
+            print("Username cannot be empty")
+            return false
+        }
+        
+        guard let email = txtemail.text else {
+            print("Email cannot be empty")
+            return false
+        }
+        guard let usermobile = txtphonenumber.text else {
+            print("Mobile number cannot be empty")
+            return false
+        }
+        guard let nibmid = txtnibmid.text else {
+            print("NIBM ID cannot be empty")
+            return false
+        }
+        guard let vehicalid = txtvehicalNumber.text else {
+            print("Vehical Number cannot be empty")
+            return false
+        }
+        guard let password = txtpassword.text else {
+            print("Password cannot be empty")
+            return false
+        }
+        
+        if username.count<5
+        {
+            print("Enter a valid Username")
+            return false
+        }
+        if email.count<10
+        {
+            print("Enter a valid Email Address")
+            return false
+        }
+        if usermobile.count<10
+        {
+            print("Enter a valid Mobile Number")
+            return false
+        }
+        if nibmid.count<12
+        {
+            print("Enter a valid NIBM ID")
+            return false
+        }
+        if vehicalid.count<5
+        {
+            print("Enter a valid Vehical ID")
+            return false
+        }
+        if password.count<5
+        {
+            print("Enter a valid Password")
+            return false
+        }
+        
+        return true
+    }
 
 }
