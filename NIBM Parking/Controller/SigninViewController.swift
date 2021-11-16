ec3905ba0980085768deb2047a5ab6f531474ce2
Ruchira Wishwajith <ruchiraherath8@gmail.com>
2021-11-16 19:43:20 +0530

    Firebase authentication for Login Page

--- NIBM Parking/Controller/SigninViewController.swift
+++ NIBM Parking/Controller/SigninViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SigninViewController: UIViewController
 {
@@ -23,15 +24,54 @@ class SigninViewController: UIViewController
     
 
     @IBAction func onSignInPressed(_ sender: UIButton) {
-        
-        if txtEmail.text?.count < 5
-        {
-            print ("Please Enter a Valid Password!!")
+        if inputeValidation(){
+            userAuthentication(password: txtPassword.text!, email: txtEmail.text!)
+        }
+        else{
+            print("Invalid data found in the given details")
+        }
+    }
+    //Validate input using optional bibding
+    func inputeValidation() -> Bool
+    {
+        guard let useremail = txtEmail.text else {
+            print("Email cannot be Empty")
+            return false
         }
         
+        guard let userpassword = txtPassword.text else {
+            print("Password Cannot Be Empty")
+            return false
+        }
         
-        
+        if useremail.count < 10
+        {
+            print("Please Enter a valid email")
+            return false
+        }
+        if userpassword.count < 8
+        {
+            print("Password lenght cannot be less than 8 characters")
+            return false
+        }
+        return true
     }
     
-  
+    func userAuthentication (password: String, email: String)
+    {
+        Auth.auth().signIn(withEmail: email, password: password)
+        {
+            authResult, error in
+            if let error = error
+            {
+                print(error.localizedDescription)
+                return
+            }
+            if let output = authResult
+            {
+                print("Email: \(output.user.email ?? "is failed to retrive")")
+                
+            }
+        }
+    }
 }
