f0a4058798256e026f1e13762630558448508dad
Ruchira Wishwajith <ruchiraherath8@gmail.com>
2021-11-17 17:16:37 +0530

    Changed MobileNumber Validation Regex Pattern

--- NIBM Parking/Util/inputValidator.swift
+++ NIBM Parking/Util/inputValidator.swift
@@ -13,9 +13,9 @@ class inputValidator
     //Regular Expressions
     static let emailRegEx = "[A-Z0-9a-z.%+-]+@[A-Z0-9a-z.-]+\\.[A-Za-z]{2,64}"
     static let nameRegEx = "[A-Za-z ]{2,100}"
-    static let mobileRegEx = "^(07)(0|1|2|3|4|5|6|7|8)[\\d]{7}S"
+    static let mobileRegEx = "^(?:0|94|\\+94)?(?:(11|21|23|24|25|26|27|31|32|33|34|35|36|37|38|41|45|47|51|52|54|55|57|63|65|66|67|81|912)(0|2|3|4|5|7|9)|7(0|1|2|5|6|7|8)\\d)\\d{6}$"
     static let nibmidRegEx = "[A-Z0-9a-z.%+-]+@[A-Z0-9a-z.-]+\\.[A-Za-z]{2,64}"
-    static let vehicleRegEx = "[A-Za-z ]{2,100}"
+    static let vehicleRegEx = "[A-Za-z ]{2,7}"
     
     //Check Email Validty
     static func emailValidity (email: String) -> Bool{
