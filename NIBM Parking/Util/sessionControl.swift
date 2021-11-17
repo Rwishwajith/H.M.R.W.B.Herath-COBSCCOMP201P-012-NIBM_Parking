7ebc8e050f56edc5900d22a59c9e253725bfeca9
Ruchira Wishwajith <ruchiraherath8@gmail.com>
2021-11-17 17:45:51 +0530

    Created UserLoginSave and UserDataRetrive methods to save user sessions

--- NIBM Parking/Util/sessionControl.swift
+++ NIBM Parking/Util/sessionControl.swift
@@ -20,8 +20,24 @@ class sessionControl
         UserDefaults.standard.setValue(true, forKey: "IS_LOGGED")
     }
     
-    func userLoginSave()
+    func userLoginSave(user : PakingUser)
     {
         UserDefaults.standard.setValue(true, forKey: "IS_LOGGED")
+        UserDefaults.standard.setValue(user.Username, forKey: "IS_NAME")
+        UserDefaults.standard.setValue(user.UserEmail, forKey: "IS_EMAIL")
+        UserDefaults.standard.setValue(user.Usermobilenumber, forKey: "IS_MOBILENO")
+        UserDefaults.standard.setValue(user.NIBMRegNumber, forKey: "IS_NIBMID")
+        UserDefaults.standard.setValue(user.VehicalNumber, forKey: "IS_VEHICALNO")
+    }
+    
+    func userDataRetrive() -> PakingUser
+    {
+        let user = PakingUser(Username: UserDefaults.standard.string(forKey: "IS_NAME") ?? "",
+                              UserEmail: UserDefaults.standard.string(forKey: "IS_EMAIL") ?? "",
+                              Usermobilenumber: UserDefaults.standard.string(forKey: "IS_MOBILENO") ?? "",
+                              UserPassword: "",
+                              NIBMRegNumber: UserDefaults.standard.string(forKey: "IS_NIBMID") ?? "",
+                              VehicalNumber: UserDefaults.standard.string(forKey: "IS_VEHICALNO") ?? "")
+        return user
     }
 }
