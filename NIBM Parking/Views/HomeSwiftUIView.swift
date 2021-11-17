9bf5245c46a61f716b5822653f40f5c962294d7e
Ruchira Wishwajith <ruchiraherath8@gmail.com>
2021-11-18 02:05:26 +0530

    Created: Home SwiftView

---
+++ NIBM Parking/Views/HomeSwiftUIView.swift
@@ -0,0 +1,24 @@
+//
+//  HomeSwiftUIView.swift
+//  NIBM Parking
+//
+//  Created by Ruchira Wishwajith on 2021-11-17.
+//
+
+import SwiftUI
+
+struct HomeSwiftUIView: View {
+    var body: some View {
+        NavigationView
+        {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).padding()
+        }
+        
+    }
+}
+
+struct HomeSwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeSwiftUIView()
+    }
+}
