4e100eeacff0fb33f93cbc2828ea54f3f7f6fb1b
Ruchira Wishwajith <ruchiraherath8@gmail.com>
2021-11-19 15:08:59 +0530

    SwiftUI for Settings application

--- NIBM Parking/Views/SettingsSwiftUIView.swift
+++ NIBM Parking/Views/SettingsSwiftUIView.swift
@@ -7,9 +7,45 @@
 
 import SwiftUI
 
+
+
 struct SettingsSwiftUIView: View {
+    @State  var firstName = ""
+    @State  var emailAddress = ""
+    @State  var mobileNumber = ""
+    @State  var nibmEmail = ""
+    @State  var vehicalNumber = ""
+    @State  var vehicalType = ""
+    @State  var showDetails = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView
+        {
+            Form {
+                Section(header: Text("Personal Information"))
+                    {
+                    Label("Username          :", systemImage: "person.circle.fill")
+                    Label("Email                   :", systemImage: "envelope.fill")
+                    Label("Mobile Number :", systemImage: "phone.fill")
+                    Label("NIBM Email        :", systemImage: "envelope.circle.fill")
+                    Label("Vehical Number :", systemImage: "car.fill")
+                }
+                Section(header: Text("Other Information"))
+                    {
+                    Button("SignOut")
+                        {
+                        showDetails.toggle()
+                    }
+                    if showDetails {
+                                    Text("Signing out..")
+                                }
+                }
+                
+            }
+            .navigationTitle("Settings")
+        }
+       
+       
     }
 }
 
