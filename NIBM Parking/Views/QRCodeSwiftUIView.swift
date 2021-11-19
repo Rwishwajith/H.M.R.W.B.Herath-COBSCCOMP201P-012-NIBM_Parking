33f17330116ebdd67adaeb6c0a34f55a164e9ed8
Ruchira Wishwajith <ruchiraherath8@gmail.com>
2021-11-19 11:15:54 +0530

    QR Code Scanner Code

---
+++ NIBM Parking/Views/QRCodeSwiftUIView.swift
@@ -0,0 +1,53 @@
+//
+//  QRCodeSwiftUIView.swift
+//  NIBM Parking
+//
+//  Created by Ruchira Wishwajith on 2021-11-19.
+//
+
+import SwiftUI
+import CodeScanner
+import Loaf
+
+struct QRCodeSwiftUIView: View {
+  @State var isPresentingScanner = false
+  @State var scannedCode: String = "Scan a QR Code to get Started"
+    
+    var scannerSheet : some View{
+        CodeScannerView(
+            codeTypes: [.qr],
+            completion: {
+                result in
+                if case let .success(code) = result
+                {
+                    self.scannedCode = code
+                    self.isPresentingScanner = false
+                    
+                }
+            }
+       )
+    }
+    
+    var body: some View {
+        VStack(spacing: 10)
+            {
+            Text(scannedCode)
+            
+            Button("Sacn QR Code")
+            {
+                self.isPresentingScanner = true
+            }
+            .sheet(isPresented: $isPresentingScanner){
+                self.scannerSheet
+            }
+        }
+    }
+}
+
+
+
+struct QRCodeSwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        QRCodeSwiftUIView()
+    }
+}
