3aff06335a110f06fed2afe939602a34d09ca572
Ruchira Wishwajith <ruchiraherath8@gmail.com>
2021-11-19 16:02:27 +0530

    TEST: Connecting UIKit with SWIFT UI

--- NIBM Parking/Controller/HomeViewController.swift
+++ NIBM Parking/Controller/HomeViewController.swift
@@ -6,18 +6,30 @@
 //
 
 import UIKit
+import SwiftUI
 
 class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let button = UIButton (frame: CGRect(x: 0, y: 0, width:220, height:50))
+        view.addSubview(button)
+        button.center = view.center
+        button.setTitle("Settings", for: .normal)
+        button.backgroundColor = .systemRed
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
         // Do any additional setup after loading the view.
     }
     
     @IBAction func OnProfilePressed(_ sender: UIButton) {
     self.performSegue(withIdentifier: "HometoProfile", sender: nil)
     }
-    
+    //Connecting Swift UI
+    @objc func didTapButton()
+    {
+        let vc = UIHostingController(rootView: SettingsSwiftUIView())
+        present(vc, animated: true)
+        
+    }
    
 }
